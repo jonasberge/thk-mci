598f12876f67c59eeeeb24f033d06b136ffc100f
vonas <24623262+vonas@users.noreply.github.com>
2020-07-08 22:38:29 +0200

    Populate tables with database data

--- docs/assets/scripts/util.js
+++ docs/assets/scripts/util.js
@@ -26,6 +26,26 @@ function toBinString(arr) {
 }
 
 
+// https://stackoverflow.com/a/6312999/6748004
+function secondsToTime(secs)
+{
+  secs = Math.round(secs);
+  var hours = Math.floor(secs / (60 * 60));
+
+  var divisor_for_minutes = secs % (60 * 60);
+  var minutes = Math.floor(divisor_for_minutes / 60);
+
+  var divisor_for_seconds = divisor_for_minutes % 60;
+  var seconds = Math.ceil(divisor_for_seconds);
+
+  return {
+    "h": hours,
+    "m": minutes,
+    "s": seconds
+  };
+}
+
+
 function resolvePath(path) {
   const root = '/thk-mci';
   const domains = [ 'vonas.github.io', 'abla.ge' ];
